c52759e44203e44734a0fa05f34abb2c188f2999
Valdomiro Morais <valdomiromorais@gmail.com>
2025-04-26 10:39:09 -0300

    cmt04 - Start player settings and making templates of waepons

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Brotegg, the brotato clone
 Brotegg is a clone of Brotato developed for academic purposes at the Instituto Federal do Sertão Pernambucano - Brazil.
+
+Free assets were used:
+1 - Scribble Dungeons (https://kenney.nl/assets/scribble-dungeons)
+2 - Font Kaph (https://ggbot.itch.io/kaph-font)
+3 - Basic GUI (https://penzilla.itch.io/basic-gui-bundle)
---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+class_name Player extends CharacterBody2D
+
+@export_category("Player Settings")
+@export
+var move_speed: float = 256.0
+
+func _physics_process(_delta: float) -> void:
+	move()
+
+func move() -> void:
+	var direction:= Vector2( Input.get_axis("move_left","move_right"), # X axis
+				Input.get_axis("move_up","move_down") ).normalized()   # Y axis
+	
+	velocity = direction * move_speed
+	
+	move_and_slide()
